d676f8f1045694b8d8bf71c15a289c606fe46f4e
Taru Ghiya <30281145+tarughiya@users.noreply.github.com>
2019-11-26 15:17:07 +0530

    changes
    changes

--- app/src/main/java/com/example/heyvisitor/HostActivity.java
+++ app/src/main/java/com/example/heyvisitor/HostActivity.java
@@ -1,6 +1,7 @@
 package com.example.heyvisitor;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,18 +11,34 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.heyvisitor.Model.Host;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HostActivity extends AppCompatActivity {
     EditText name, email, phone;
     Button hostbutton;
 
-    FirebaseDatabase database;
-    DatabaseReference hostdatabase;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    // [END get_firestore_instance]
+
+    // [START set_firestore_settings]
+    FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+            .setPersistenceEnabled(true)
+            .build();
+
+//    FirebaseDatabase database;
+//    DatabaseReference hostdatabase;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +49,8 @@ public class HostActivity extends AppCompatActivity {
         phone = findViewById(R.id.phone);
         hostbutton = findViewById(R.id.hostbutton);
 
-        database = FirebaseDatabase.getInstance();
-        hostdatabase = database.getReference();
+//        database = FirebaseDatabase.getInstance();
+//        hostdatabase = database.getReference();
 
         hostbutton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,23 +65,44 @@ public class HostActivity extends AppCompatActivity {
                     alert.setMessage("Fill up all the fields!");
                     alert.show();
                 } else {
-                    hostdatabase.addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                            Host host = new Host(Name, Email, Number);
-                            hostdatabase.child(Name).setValue(host);
 
-                            AlertDialog.Builder alert = new AlertDialog.Builder(HostActivity.this);
-                            alert.setTitle("Success");
-                            alert.setMessage("Host added successfully!");
-                            alert.show();
-                        }
+                    Map<String, Object> host = new HashMap<>();
+                    host.put("Name", Name);
+                    host.put("Email", Email);
+                    host.put("Number",Number);
 
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                    db.collection("Host")
+                            .add(host)
+                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                @Override
+                                public void onSuccess(DocumentReference documentReference) {
+                                    AlertDialog.Builder alert = new AlertDialog.Builder(HostActivity.this);
+                                    alert.setTitle("Success");
+                                    alert.setMessage("Host added successfully!");
+                                    alert.show();
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+//                                    Log.w(TAG, "Error adding document", e);
+                                }
+                            });
 
-                        }
-                    });
+//                    hostdatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+//                        @Override
+//                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                            Host host = new Host(Name, Email, Number);
+//                            hostdatabase.child(Name).setValue(host);
+//
+
+//                        }
+//
+//                        @Override
+//                        public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//                        }
+//                    });
                 }
             }
         });
--- app/src/main/java/com/example/heyvisitor/VisitorActivity.java
+++ app/src/main/java/com/example/heyvisitor/VisitorActivity.java
@@ -1,6 +1,7 @@
 package com.example.heyvisitor;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -14,11 +15,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import com.example.heyvisitor.Model.Visitor;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.sql.Time;
 import java.text.DateFormat;
@@ -26,7 +36,9 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.TimeZone;
 
 
@@ -36,9 +48,17 @@ public class VisitorActivity extends AppCompatActivity {
    // TextView textView;
     Button checkin;
 
-    FirebaseDatabase database;
-    DatabaseReference visitordatabase;
-    DatabaseReference hostdatabase;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    // [END get_firestore_instance]
+
+    // [START set_firestore_settings]
+    FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+            .setPersistenceEnabled(true)
+            .build();
+
+//    FirebaseDatabase database;
+//    DatabaseReference visitordatabase;
+//    DatabaseReference hostdatabase;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,17 +70,19 @@ public class VisitorActivity extends AppCompatActivity {
         checkin=findViewById(R.id.checkin);
 
 
-        database=FirebaseDatabase.getInstance();
-        visitordatabase= database.getReference();
-        hostdatabase= database.getReference();
+//        database=FirebaseDatabase.getInstance();
+//        visitordatabase= database.getReference();
+//        hostdatabase= database.getReference();
 
 
         checkin.setOnClickListener(new View.OnClickListener() {
+            private static final String TAG = "";
+
             @Override
             public void onClick(View v) {
                 final String VName = vname.getText().toString().trim();
                 final String VEmail = vemail.getText().toString().trim();
-                final int VNumber = 0; //Integer.parseInt(vphone.getText().toString());
+                final int VNumber = 0;//Integer.parseInt(vphone.getText().toString());
 
 
 
@@ -72,58 +94,119 @@ public class VisitorActivity extends AppCompatActivity {
 
                 final String localTime = date.format(currentLocalTime);
 
-                hostdatabase.child("Name").addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        // Is better to use a List, because you don't know the size
-                        // of the iterator returned by dataSnapshot.getChildren() to
-                        // initialize the array
-                        final List<String> host = new ArrayList<String>();
 
-                        for (DataSnapshot hostSnapshot : dataSnapshot.getChildren()) {
-                            String hostname = hostSnapshot.child("hostName").getValue(String.class);
-                            host.add(hostname);
-                        }
 
-                        hostname = findViewById(R.id.hostname);
-                        ArrayAdapter<String> areasAdapter = new ArrayAdapter<String>(VisitorActivity.this, android.R.layout.simple_spinner_item, host );
+                db.collection("Host")
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        //Log.d(TAG, document.getId() + " => " + document.getData());
+                                        final List<String> host = new ArrayList<String>();
+                                        host.add(document.getData().toString());
+                                        hostname = findViewById(R.id.hostname);
+                       ArrayAdapter<String> areasAdapter = new ArrayAdapter<String>(VisitorActivity.this, android.R.layout.simple_spinner_item, host );
                         areasAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                         hostname.setAdapter(areasAdapter);
-
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-
-                    }
-                });
+                                        areasAdapter.add(host.toString());
+                                        areasAdapter.notifyDataSetChanged();
+                                    }
+                                } else {
+                                    Log.d(TAG, "Error getting documents: ", task.getException());
+                                }
+                            }
+                        });
                 final String role = hostname.getSelectedItem().toString();
-
 
                 if (VName.isEmpty() || VEmail.isEmpty()) {
                     AlertDialog.Builder alert = new AlertDialog.Builder(VisitorActivity.this);
                     alert.setTitle("Failure");
                     alert.setMessage("Fill up all the fields!");
-                    alert.show();
-                } else {
-                    visitordatabase.addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                            Visitor visitor = new Visitor(VName, VEmail, VNumber, localTime,role );
-                            visitordatabase.child(VName).setValue(visitor);
+                    alert.show();}
+                else
+                {
+                    Map<String, Object> visitor = new HashMap<>();
+                    visitor.put("Name", VName);
+                    visitor.put("Email", VEmail);
+                    visitor.put("Number",VNumber);
+                    visitor.put("Time",localTime);
+                    visitor.put("HostNmae",role);
 
-                            AlertDialog.Builder alert = new AlertDialog.Builder(VisitorActivity.this);
-                            alert.setTitle("Success");
-                            alert.setMessage("Visitor added successfully!");
-                            alert.show();
-                        }
-
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                        }
-                    });
+                    db.collection("Visitor")
+                            .add(visitor)
+                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                @Override
+                                public void onSuccess(DocumentReference documentReference) {
+                                    AlertDialog.Builder alert = new AlertDialog.Builder(VisitorActivity.this);
+                                    alert.setTitle("Success");
+                                    alert.setMessage("Visitor added successfully!");
+                                    alert.show();
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+//                                    Log.w(TAG, "Error adding document", e);
+                                }
+                            });
                 }
+
+
+
+//                hostdatabase.child("Name").addValueEventListener(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(DataSnapshot dataSnapshot) {
+//                        // Is better to use a List, because you don't know the size
+//                        // of the iterator returned by dataSnapshot.getChildren() to
+//                        // initialize the array
+//                        final List<String> host = new ArrayList<String>();
+//
+//                        for (DataSnapshot hostSnapshot : dataSnapshot.getChildren()) {
+//                            String hostname = hostSnapshot.child("hostName").getValue(String.class);
+//                            host.add(hostname);
+//                        }
+//
+//                        hostname = findViewById(R.id.hostname);
+//                        ArrayAdapter<String> areasAdapter = new ArrayAdapter<String>(VisitorActivity.this, android.R.layout.simple_spinner_item, host );
+//                        areasAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//                        hostname.setAdapter(areasAdapter);
+//
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(DatabaseError databaseError) {
+//
+//                    }
+//                });
+//                final String role = hostname.getSelectedItem().toString();
+
+
+//                if (VName.isEmpty() || VEmail.isEmpty()) {
+//                    AlertDialog.Builder alert = new AlertDialog.Builder(VisitorActivity.this);
+//                    alert.setTitle("Failure");
+//                    alert.setMessage("Fill up all the fields!");
+//                    alert.show();
+//                } else {
+//                    visitordatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+//                        @Override
+//                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                            Visitor visitor = new Visitor(VName, VEmail, VNumber, localTime,role );
+//                            visitordatabase.child(VName).setValue(visitor);
+//
+//                            AlertDialog.Builder alert = new AlertDialog.Builder(VisitorActivity.this);
+//                            alert.setTitle("Success");
+//                            alert.setMessage("Visitor added successfully!");
+//                            alert.show();
+//                        }
+//
+//                        @Override
+//                        public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//                        }
+//                    });
+//                }
             }});
 
 
